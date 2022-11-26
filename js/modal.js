314884baaf199cee5059c276c307b091364cef5a
Volodymyr Smetana <vilsmet@gmail.com>
2022-11-26 01:19:12 +0100

    first draft

--- js/modal.js
+++ js/modal.js
@@ -1,4 +1,5 @@
 
+(() => {
   const refs = {
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
@@ -11,3 +12,24 @@
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
   }
+})();
+
+
+// (() => {
+//     const refs = {
+//         openModalBtn: document.querySelector("[data-open-modal]"),
+//         closeModalBtn: document.querySelector("[data-close-modal]"),
+//         backdrop: document.querySelector("[data-backdrop]"),
+//     };
+//     refs.openModalBtn.addEventListener("click", toggleModal);
+//     refs.closeModalBtn.addEventListener("click", toggleModal);
+//     refs.backdrop.addEventListener("click", logBackdropClick);
+
+//     function toggleModal () {
+//         refs.backdrop.classList.toggle("is-hidden");
+//     }
+
+//     function logBackdropClick() {
+//         console.log("click to backdrop")
+//     }
+// })();
